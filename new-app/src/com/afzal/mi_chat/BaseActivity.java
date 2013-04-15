5025d4eb9cf3eb8d995646cc4e69609728285e40
Afzal Najam <afzalnaj@gmail.com>
2013-04-15 04:20:47 -0400

    MessagesActivity with SlidingMenu
    updated android-support-v4.jar
    Added BaseActivity with SlidingMenu
    Added UserList in the SlidingMenu

---
+++ new-app/src/com/afzal/mi_chat/BaseActivity.java
@@ -0,0 +1,50 @@
+package com.afzal.mi_chat;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.ListFragment;
+
+import com.actionbarsherlock.view.MenuItem;
+import com.slidingmenu.lib.SlidingMenu;
+import com.slidingmenu.lib.app.SlidingFragmentActivity;
+
+public class BaseActivity extends SlidingFragmentActivity {
+    protected ListFragment mFrag;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setTitle(R.string.app_name);
+
+        setBehindContentView(R.layout.menu_frame);
+        if (savedInstanceState == null) {
+            FragmentTransaction t = this.getSupportFragmentManager().beginTransaction();
+            mFrag = new UserListFragment();
+            t.replace(R.id.menu_frame, mFrag);
+            t.commit();
+        } else {
+            mFrag = (ListFragment) this.getSupportFragmentManager().findFragmentById(R.id.menu_frame);
+        }
+
+        SlidingMenu sm = getSlidingMenu();
+
+        sm.setShadowWidthRes(R.dimen.shadow_width);
+        sm.setShadowDrawable(R.drawable.shadow);
+        sm.setBehindOffsetRes(R.dimen.slidingmenu_offset);
+        sm.setFadeDegree(0.45f);
+        sm.setTouchModeAbove(SlidingMenu.TOUCHMODE_MARGIN);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                toggle();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
