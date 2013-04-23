e24d454e6928b8cf864c212623425dcf067177d6
Afzal Najam <afzalnaj@gmail.com>
2013-04-23 01:24:28 -0400

    Preliminary Google Http Client implementation
    Bug: can't login

--- new-app/src/com/afzal/mi_chat/LoginActivity.java
+++ new-app/src/com/afzal/mi_chat/LoginActivity.java
@@ -1,16 +1,30 @@
 package com.afzal.mi_chat;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.graphics.Typeface;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.method.PasswordTransformationMethod;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.afzal.mi_chat.Utils.NetUtils;
+import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpHeaders;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpResponse;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.UrlEncodedContent;
 
 public class LoginActivity extends Activity {
 
@@ -40,10 +54,59 @@ public class LoginActivity extends Activity {
                 hideKeyboard();
                 String username = mUsernameField.getText().toString();
                 String password = mPasswordField.getText().toString();
-                Intent i = new Intent(LoginActivity.this, MessagesActivity.class);
-                startActivity(i);
+                new LoginTask().execute(username, password);
+                //                Intent i = new Intent(LoginActivity.this, MessagesActivity.class);
+                //                startActivity(i);
             }
         });
+
+    }
+
+    public class LoginTask extends AsyncTask<String, Void, String> {
+
+        private final String TAG = LoginTask.class.getSimpleName();
+
+        @SuppressWarnings("unchecked")
+        @Override
+        protected String doInBackground(String... params) {
+            Map<String, String> paramsMap = new HashMap<String, String>();
+            paramsMap.put("vb_login_username", params[0]);
+            paramsMap.put("vb_login_password", params[1]);
+            paramsMap.put("do", "login");
+
+            HttpTransport transport = NetUtils.getTransport();
+            HttpRequest request;
+            HttpResponse response;
+            String result = new String();
+            try {
+
+                // TODO fix login not working, bbpassword cookie is not returned by the server
+                request = transport.createRequestFactory().buildPostRequest(new GenericUrl("http://www.macinsiders.com/login.php"), new UrlEncodedContent(params));
+//                request = transport.createRequestFactory().buildGetRequest(new GenericUrl("http://www.macinsiders.com/chat/?ajax=true"));
+                response = request.execute();
+
+//                String content = CharStreams.toString(new InputStreamReader(response.getContent()));
+//                Log.d(TAG, content);
+
+                HttpHeaders headers = response.getHeaders();
+                ArrayList<String> cookies = (ArrayList<String>) headers.get("set-cookie");
+                for (String cookie : cookies) {
+                    Log.d(TAG, cookie);
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return result;
+
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            Log.d(TAG, result);
+        }
 
     }
 
