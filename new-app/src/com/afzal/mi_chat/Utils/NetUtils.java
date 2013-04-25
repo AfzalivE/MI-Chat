4a5c84f1f59650eb52e3b8eb41c40e90b4280d6d
Afzal Najam <afzalnaj@gmail.com>
2013-04-25 03:32:09 -0400

    Capture xml response to handle it
    Forms an XML Document with the response body
    Changed ListView ids in layouts
    Implemented a Persistent cookie store to have the cookies saved in preferences for later sessions even after closing the app
    Improved login screen UI and handled errors

--- new-app/src/com/afzal/mi_chat/Utils/NetUtils.java
+++ new-app/src/com/afzal/mi_chat/Utils/NetUtils.java
@@ -1,9 +1,19 @@
 package com.afzal.mi_chat.Utils;
 
+import android.content.Context;
+
 import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.PersistentCookieStore;
 
 public class NetUtils {
 
     public static AsyncHttpClient client = new AsyncHttpClient();
+    private static PersistentCookieStore myCookieStore;
 
+    public static synchronized PersistentCookieStore getCookieStoreInstance(Context context) {
+        if (myCookieStore == null) {
+            myCookieStore = new PersistentCookieStore(context);
+        }
+        return myCookieStore;
+    }
 }
