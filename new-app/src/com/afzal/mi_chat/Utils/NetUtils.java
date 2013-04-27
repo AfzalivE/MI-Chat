cc2e1d4abdd7d2401deb232c39aa25e2c5f31ec6
Afzal Najam <afzalnaj@gmail.com>
2013-04-27 04:25:00 -0400

    Allow circular redirects

--- new-app/src/com/afzal/mi_chat/Utils/NetUtils.java
+++ new-app/src/com/afzal/mi_chat/Utils/NetUtils.java
@@ -1,5 +1,7 @@
 package com.afzal.mi_chat.Utils;
 
+import org.apache.http.client.params.ClientPNames;
+
 import android.content.Context;
 
 import com.loopj.android.http.AsyncHttpClient;
@@ -7,8 +9,17 @@ import com.loopj.android.http.PersistentCookieStore;
 
 public class NetUtils {
 
-    public static AsyncHttpClient client = new AsyncHttpClient();
+    public static AsyncHttpClient client;
     private static PersistentCookieStore myCookieStore;
+
+    public static synchronized AsyncHttpClient getClientInstance() {
+        if (client == null) {
+            client = new AsyncHttpClient();
+        }
+
+        client.getHttpClient().getParams().setParameter(ClientPNames.ALLOW_CIRCULAR_REDIRECTS, true);
+        return client;
+    }
 
     public static synchronized PersistentCookieStore getCookieStoreInstance(Context context) {
         if (myCookieStore == null) {
