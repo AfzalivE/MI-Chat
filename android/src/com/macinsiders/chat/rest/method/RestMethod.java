a864c0d6710a9008630124d626a6a0343c8f291f
Afzal Najam <afzalnaj@gmail.com>
2013-05-26 12:19:43 -0400

    Removed the old app folder
    Min SDK is now 8 (Android 2.2)
    But this isn't the version on the Play Store

--- android/src/com/macinsiders/chat/rest/method/RestMethod.java
+++
@@ -1,8 +0,0 @@
-package com.macinsiders.chat.rest.method;
-
-import com.macinsiders.chat.resource.Resource;
-
-public interface RestMethod<T extends Resource>{
-
-    public RestMethodResult<T> execute(); // might throw UnknownHostException, something for later
-}
