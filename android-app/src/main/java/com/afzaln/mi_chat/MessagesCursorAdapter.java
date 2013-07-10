9f00d27c8d08f8cc7de518b77467bc759b3c2a8a
Afzal Najam <afzalnaj@gmail.com>
2013-07-10 00:33:42 -0400

    More user actions added
    List background colour adjusted to have the same colour throughout
    Slight action_list_item layout changes

--- android-app/src/main/java/com/afzaln/mi_chat/MessagesCursorAdapter.java
+++ android-app/src/main/java/com/afzaln/mi_chat/MessagesCursorAdapter.java
@@ -124,20 +124,47 @@ public class MessagesCursorAdapter extends CursorAdapter {
 
     private String formatMessage(String userName, String message) {
         if (message.startsWith("/login")) {
-            return message.replace("/login", "Login: ");
+            return message.replace("/login", "").concat(" logged in");
         }
 
         if (message.startsWith("/logout")) {
-            return message.replace("/logout", "Logout: ")
-                          .replace("Timeout", "(timeout)");
+            if (message.contains("Timeout")) {
+                return message.replace("/logout", "").replace("Timeout", "logged out (timeout)");
+            } else {
+                return message.replace("/logout", "").concat(" logged out");
+            }
         }
 
         if (message.startsWith("/roll")) {
-            return message.replace("/roll", "Roll: ");
+            return message.replace("/roll", "Roll:");
         }
 
         if (message.startsWith("/me")) {
-            return message.replace("/me", userName + " ");
+            return message.replace("/me", userName);
+        }
+
+        if (message.startsWith("/privmsgto")) {
+            return message.replace("/privmsgto", "Whisper to");
+        }
+
+        if (message.startsWith("/privmsg")) {
+            return message.replace("/privmsg", "Whisper from " + userName);
+        }
+
+        if (message.startsWith("/privactionto")) {
+            return message.replace("/privactionto", "Action to");
+        }
+
+        if (message.startsWith("/privaction")) {
+            return message.replace("/privaction", "Action from " + userName);
+        }
+
+        if (message.startsWith("/queryOpen")) {
+            return message.replace("/queryOpen", "Private channel opened to");
+        }
+
+        if (message.startsWith("/queryClose")) {
+            return message.replace("/queryClose", "Private channel to ").concat(" closed");
         }
 
         return message;
