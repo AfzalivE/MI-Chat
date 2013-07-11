f2fa56f26fe15a9190f1d1cb40180b65f458d251
Afzal Najam <afzalnaj@gmail.com>
2013-07-10 22:55:13 -0400

    Refactor, Added dummy PreferencesActivity
    Added initial PrefUtils

---
+++ android-app/src/main/java/com/afzaln/mi_chat/activities/PreferencesActivity.java
@@ -0,0 +1,43 @@
+package com.afzaln.mi_chat.activities;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+import com.actionbarsherlock.app.SherlockPreferenceActivity;
+import com.actionbarsherlock.view.MenuItem;
+import com.afzaln.mi_chat.R;
+
+public class PreferencesActivity extends SherlockPreferenceActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getSupportActionBar().setTitle(R.string.action_prefs);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    public class MyPreferenceFragment extends PreferenceFragment {
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            addPreferencesFromResource(R.xml.preferences);
+        }
+    }
+
+}
+
---
+++ android-app/src/main/java/com/afzaln/mi_chat/utils/PrefUtils.java
@@ -0,0 +1,55 @@
+package com.afzaln.mi_chat.utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+public class PrefUtils {
+
+    /**
+     * Commits a preference in SharedPreferences.
+     *
+     * @param context
+     *            the context
+     * @param stringName
+     *            the preference name
+     * @param stringValue
+     *            the value of the preference
+     */
+
+    public static void setPref(Context context, String stringName, String stringValue) {
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor prefsEditor = sharedPrefs.edit();
+        prefsEditor.putString(stringName, stringValue);
+        prefsEditor.commit();
+    }
+
+    /**
+     * Returns the value of a preference in SharedPreferences.
+     *
+     * @param context
+     *            the context
+     * @param stringName
+     *            the preference name
+     * @return stringValue the value of the preference
+     */
+
+    public static boolean getBoolPref(Context context, String stringName) {
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+        boolean prefValue = sharedPrefs.getBoolean(stringName, false);
+        return prefValue;
+    }
+
+    /**
+     * Removes all preferences from SharedPreferences
+     *
+     * @param context
+     *            the context
+     */
+    public static void clearPrefs(Context context) {
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor prefsEditor = sharedPrefs.edit();
+        prefsEditor.clear();
+        prefsEditor.commit();
+    }
+}
