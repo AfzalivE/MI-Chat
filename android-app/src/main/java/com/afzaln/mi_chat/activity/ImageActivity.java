c39df74541e993e525dd8802f49899fbec4ee7a4
Afzal Najam <afzalnaj@gmail.com>
2013-08-23 00:56:04 -0400

    Initial ImageActivity to display large images

---
+++ android-app/src/main/java/com/afzaln/mi_chat/activity/ImageActivity.java
@@ -0,0 +1,18 @@
+package com.afzaln.mi_chat.activity;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.afzaln.mi_chat.R;
+
+/**
+ * Created by afzal on 2013-08-23.
+ */
+public class ImageActivity extends FragmentActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.image_activity_layout);
+    }
+}
