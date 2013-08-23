bbda5bdd2c82d16948f9d0c9e8b69702356bfc1d
Afzal Najam <afzalnaj@gmail.com>
2013-08-23 01:12:39 -0400

    Show the first image from attached images in ImageActivity

--- android-app/src/main/java/com/afzaln/mi_chat/activity/ImageActivity.java
+++ android-app/src/main/java/com/afzaln/mi_chat/activity/ImageActivity.java
@@ -2,8 +2,12 @@ package com.afzaln.mi_chat.activity;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.widget.ImageView;
 
 import com.afzaln.mi_chat.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
 
 /**
  * Created by afzal on 2013-08-23.
@@ -14,5 +18,15 @@ public class ImageActivity extends FragmentActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.image_activity_layout);
+
+        ArrayList<String> imgLinksList = getIntent().getExtras().getStringArrayList("imgLinksList");
+
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+
+        Picasso.with(this)
+                .load(imgLinksList.get(0))
+                .placeholder(R.drawable.placeholder)
+                .error(R.drawable.error)
+                .into(imageView);
     }
 }
