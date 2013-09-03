381dd3cb9790f73b5a4dc85814cc789067fdf3c2
Afzal Najam <afzalnaj@gmail.com>
2013-09-03 17:35:31 -0400

    Default value for loggedIn to avoid NPE

--- android-app/src/main/java/com/afzaln/mi_chat/resource/Info.java
+++ android-app/src/main/java/com/afzaln/mi_chat/resource/Info.java
@@ -11,7 +11,7 @@ public class Info {
     int channelId;
     String userName;
     String channelName;
-    boolean loggedIn;
+    boolean loggedIn = false;
 
     public Info(long userId, int userRole, int channelId, String userName, String channelName, boolean loggedIn) {
         this.userId = userId;
