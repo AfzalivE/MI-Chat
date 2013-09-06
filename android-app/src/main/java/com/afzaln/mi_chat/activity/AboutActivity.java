95f8e2f8e7a9322311424ca6bb7a6c9275c0f7dc
Afzal Najam <afzalnaj@gmail.com>
2013-09-05 21:50:38 -0400

    Center AboutActivity layout and extend ActionBarActivity

--- android-app/src/main/java/com/afzaln/mi_chat/activity/AboutActivity.java
+++ android-app/src/main/java/com/afzaln/mi_chat/activity/AboutActivity.java
@@ -6,6 +6,7 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
 import android.text.util.Linkify;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -16,7 +17,7 @@ import com.afzaln.mi_chat.R;
 /**
  * Created by afzal on 2013-09-03.
  */
-public class AboutActivity extends BaseActivity {
+public class AboutActivity extends ActionBarActivity {
 
     /**
      * Called when the activity is first created.
