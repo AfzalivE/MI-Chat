56948a13ec19653d39781b8ae3858bf13c4ac417
Afzal Najam <afzalnaj@gmail.com>
2014-02-27 13:58:32 -0500

    Added Crashlytics
    Doesn't apply Crashlytics plugin by default for debug builds.
    For release builds, Crashlytics API key is needed.

--- android-app/src/main/java/com/afzaln/mi_chat/MIChatApp.java
+++ android-app/src/main/java/com/afzaln/mi_chat/MIChatApp.java
@@ -5,6 +5,7 @@ import android.content.ContentResolver;
 
 import com.afzaln.mi_chat.provider.ProviderContract.MessagesTable;
 import com.afzaln.mi_chat.provider.ProviderContract.UsersTable;
+import com.crashlytics.android.Crashlytics;
 
 /**
  * Created by afzaln on 2013-05-22.
@@ -14,6 +15,9 @@ public class MIChatApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        if (!BuildConfig.DEBUG_BUILD) {
+            Crashlytics.start(this);
+        }
         ContentResolver cr = getContentResolver();
 
         cr.delete(UsersTable.CONTENT_URI, null, null);
